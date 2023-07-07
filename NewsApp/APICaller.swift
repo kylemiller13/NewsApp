7d0028e1b698615e945dbf5d0f2b0c3c9b48cf47
kylemiller13 <millerkyle85@gmail.com>
2023-07-07 10:55:49 -0700

    add code for News App

---
+++ NewsApp/APICaller.swift
@@ -0,0 +1,60 @@
+//
+//  APICaller.swift
+//  NewsApp
+//
+//  Created by Kyle Miller on 7/6/23.
+//
+
+import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants {
+        static let topHeadlinesURL = URL(string:"https://newsapi.org/v2/top-headlines?country=US&apiKey=1ca65062294949b4a6e9eb04ac951e5b")
+    }
+    private init(){}
+    
+    public func getTopStories(completion: @escaping (Result<[Article], Error>) -> Void) {
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data,  response, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    print("Articles: \(result.articles.count)")
+                    completion(.success(result.articles))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        task.resume()
+    }
+}
+
+//Models
+
+struct APIResponse: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String
+}
+
+struct Source: Codable {
+    let name: String
+}
